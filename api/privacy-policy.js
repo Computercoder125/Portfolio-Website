0d2ab6443083ed8546489f1cdae500c1ea2d17a0
Computercoder125 <sg10763150@sju.edu>
2024-12-30 14:14:13 -0500

    Create privacy-policy.js

---
+++ api/privacy-policy.js
@@ -0,0 +1,3 @@
+export default function handler(req, res) {
+  res.status(200).send("All rights are reserved for Sean Devang Gor. Distribution of any code/website content without my consent is subject to fines and/or other penalties.");
+}
